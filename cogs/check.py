e9080fadaf9b33168330d0c0218c46196d3567f8
dei <allamegod@gmail.com>
2023-02-23 19:29:43 +0300

    getting role from the group test

--- cogs/check.py
+++ cogs/check.py
@@ -33,7 +33,7 @@ class Check(commands.GroupCog, name = "check"):
         super().__init__()
 
     @app_commands.command(name = "ct", description="Make a check on the profile. Useful for before and after CT.")
-    async def ct(self, interaction: discord.Interaction, user: str):
+    async def ct(self, interaction: discord.Interaction, user: str, roleTest: bool):
         
         userid = user.lower()
         if userid.startswith("http://"):
@@ -64,6 +64,13 @@ class Check(commands.GroupCog, name = "check"):
 
         try:
             userGroups = get(f'https://groups.roblox.com/v2/users/{userid}/groups/roles').json()
+            
+            for item in userGroups['data']:
+                temporaryItem = item
+                if temporaryItem['group']['id'] == 872876:
+                    mainGroupRole = temporaryItem['role']['name']
+                    break
+                mainGroupRole = "Foreigner"       
             userGroups = [value for i in userGroups['data'] for value in i.values()]
             userGroups = [sub['id'] for sub in userGroups]
             userInfo = get(f'https://users.roblox.com/v1/users/{userid}').json()
@@ -79,7 +86,7 @@ class Check(commands.GroupCog, name = "check"):
             timeNow = datetime.datetime.now()
             userCreatedDelta = (timeNow - userCreated).days
             userName = userInfo['name']
-            enemyGroups = enemyGroups = [5248163, 4886107, 15294045, 15635299, 15815549, 15815551, 15815554, 15815556, 13529471, 13529222, 13871657, 13819322, 16139753, 13439446, 13529342, 13529439, 13529455, 9898651, 11439837, 10134262, 3962491, 2795991, 2650759, 2682451, 6521883, 16637782, 16538494]
+            enemyGroups = [5248163, 4886107, 15294045, 15635299, 15815549, 15815551, 15815554, 15815556, 13529471, 13529222, 13871657, 13819322, 16139753, 13439446, 13529342, 13529439, 13529455, 9898651, 11439837, 10134262, 3962491, 2795991, 2650759, 2682451, 6521883, 16637782, 16538494]
             mainGroup = 872876
             try:
                 enemyGroupsCounter = userGroups + enemyGroups
@@ -158,7 +165,10 @@ class Check(commands.GroupCog, name = "check"):
             embed2.add_field(name="Account Age", value=f"{(lambda: '>60 days', lambda: '__<60 DAYS__')[userCreatedDelta < 60]()}", inline=True)
             embed2.add_field(name="Enemy Groups", value=f"{(lambda: 'Clear!', lambda: '__ENEMY GROUPS DETECTED__')[enemyCounter > 0]()}", inline=False)
             embed2.add_field(name="Blacklisted", value=f"{(lambda: 'Not detected', lambda: '__BLACKLISTED__')[userName in nameValues and lifted == 'FALSE']()}", inline=True)
-            embed2.add_field(name="Ranked Foreigner:", value=f"{(lambda: '__FOREIGNER__', lambda: 'Not Foreigner')[mainGroup in userGroups]()}", inline=True)
+            if roleTest:
+                embed2.add_field(name="Rank in Main Group:", value=f"{mainGroupRole}", inline=True)
+            else:
+                embed2.add_field(name="Ranked Foreigner:", value=f"{(lambda: '__FOREIGNER__', lambda: 'Not Foreigner')[mainGroup in userGroups]()}", inline=True)
             embed2.set_thumbnail(url=userThumbnail)
             await interaction.edit_original_response(embed=embed2)
         except Exception as e:
